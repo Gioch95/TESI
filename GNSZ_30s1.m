2345232c0d689c56d13cbdf49adcc84024af62e2
Gioch95 <46966372+Gioch95@users.noreply.github.com>
2019-04-03 17:20:01 +0200

    Rename GNSZ_30s.m to GNSZ_30s1.m

--- GNSZ_30s1.m
+++ GNSZ_30s1.m
@@ -1,7 +1,7 @@
 function Y=seizurestime30(T,fs)
 load('T.mat');
 inDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZ\';
-outDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZ 30 secondi\';%qui ho inserito un \ in più
+outDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZ 30 secondi\';%qui ho inserito un \ in piÃ¹
 fs=250;
 
 estensione='*.mat';
@@ -10,12 +10,12 @@ for i=1:length(cases)
  load(strcat(inDir,cases(i).name));
 
    for j=1:(max(size(T))) 
-    if strncmpi(cases(i).name,T(j,1),41)==1 %controllo dopo
+    if strncmpi(cases(i).name,T(j,1),37)==1 %controllo dopo
               T0=T{j,2};
               T1=fs*T0;
               x=30*fs;
              Y=EEG.mydata(:,T1-x:T1-1);
-    end%a è la struttura %b è la matrice canalixcampioni %tempo di start seizure %x sono i 30 secondi
+    end%a Ã¨ la struttura %b Ã¨ la matrice canalixcampioni %tempo di start seizure %x sono i 30 secondi
         save(strcat(outDir,(strtok(cases(i).name,'.')),'30.mat'),'Y')
    end
           
