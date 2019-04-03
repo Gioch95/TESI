5269f5ced443a999e218f832bc31635659a8f6a6
Gioch95 <46966372+Gioch95@users.noreply.github.com>
2019-04-03 17:21:09 +0200

    Rename FNSZ_30s.m to FNSZ_30s1.m

--- FNSZ_30s1.m
+++ FNSZ_30s1.m
@@ -1,7 +1,7 @@
-function Y=seizurestime(T,fs)
+function Y=FNSZ_30s(filename)
 load('C.mat');
 inDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZ\';
-outDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZ 30 secondi\';%qui ho inserito un \ in più
+outDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZ 30 secondi\';%qui ho inserito un \ in piÃ¹
 fs=250;
 
 estensione='*.mat';
@@ -10,12 +10,12 @@ for i=1:length(cases)
  load(strcat(inDir,cases(i).name));
 
    for j=1:(max(size(C.t))) 
-    if strncmpi(cases(i).name,C.t(j,1),41)==1 %controllo dopo
+    if strncmpi(cases(i).name,C.t(j,1),37)==1 %controllo dopo
               T=C.t{j,2};
               T1=fs*T;
               x=30*fs;
              Y=EEG.mydata(:,T1-x:T1-1);
-    end%a è la struttura %b è la matrice canalixcampioni %tempo di start seizure %x sono i 30 secondi
+    end%a Ã¨ la struttura %b Ã¨ la matrice canalixcampioni %tempo di start seizure %x sono i 30 secondi
         save(strcat(outDir,(strtok(cases(i).name,'.')),'30.mat'),'Y')
    end
           
