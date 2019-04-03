dfa965eace95ebc4ad756db670c78f144f86fd28
Gioch95 <46966372+Gioch95@users.noreply.github.com>
2019-04-03 17:31:49 +0200

    Update JamoviPSDtemporale.m

--- JamoviPSDtemporale.m
+++ JamoviPSDtemporale.m
@@ -1,26 +1,26 @@
-function matrix=JamoviPSDtemporale(chan,fs,PSDchanFNSZ,PSDchanGNSZ)
-chan=19;%19 canali
+function matrix=JamoviPSDtemporale(chan,PSDchanFNSZ,PSDchanGNSZ)
+chan=17;%19 canali
 epoch=6;%da 5 secondi
 bande=5;%delta,gamma,beta,alpha,theta
-%qui ho inserito un \ in più
+%qui ho inserito un \ in piÃ¹
 fs=250;
 estensione='*.mat';
 %CHAN
-FN=zeros(1,epoch*bande+1);
+FN=zeros(1,chan*bande+1);
 GN=FN;
 inDirFNSZ='C:\Users\gioiachiodi\Documents\MATLAB\PSDchanFNSZ\';
 inDirGNSZ='C:\Users\gioiachiodi\Documents\MATLAB\PSDchanGNSZ\';
-outDirchan='C:\Users\gioiachiodi\Documents\MATLAB\Chan\';
+outDirchan='C:\Users\gioiachiodi\Documents\MATLAB\PSDtemporale\';
 
 cases1=dir(fullfile(inDirFNSZ,estensione));
 cases2=dir(fullfile(inDirGNSZ,estensione))
 for i=1:length(cases1)
     load(strcat(inDirFNSZ,cases1(i).name));
-    FN=[FN;"FNSZ",PSDchan(:)'];
+    FN=[FN;"FNSZ",PSDepoch(:)'];
 end
 for i=1:length(cases2)
     load(strcat(inDirGNSZ,cases2(i).name));
-    GN=[GN;"GNSZ",PSDchan(:)'];
+    GN=[GN;"GNSZ",PSDepoch(:)'];
 end 
 FN(1,:)=[];
 GN(1,:)=[];
