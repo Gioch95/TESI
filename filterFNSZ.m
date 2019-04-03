43e39d76fb744876e845dd56c1afa905f7a39b37
Gioch95 <46966372+Gioch95@users.noreply.github.com>
2019-04-03 17:39:46 +0200

    Update filterFNSZ.m

--- filterFNSZ.m
+++ filterFNSZ.m
@@ -1,17 +1,17 @@
-function EEGOUT=filterFNSZ(fs,Y)
+function EEGOUT=filterFNSZ(Y)
 
-inDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZ 30 secondi\';
-outDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZFilter\';%qui ho inserito un \ in più
+inDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZ\';
+outDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZFilter\';%qui ho inserito un \ in piÃ¹
 fs=250;
 
 estensione='*.mat';
 cases=dir(fullfile(inDir,estensione));
 for i=1:length(cases)
      load(strcat(inDir,cases(i).name));
-     EEG.mydata=Y;
-     locutoff= 10;
-     hicutoff= 40;
-    EEGOUT = eegfilt( EEG.mydata, fs, locutoff, hicutoff);
-save(strcat(outDir,(strtok(cases(i).name,'.')),'filt.mat'),'EEGOUT')
-
+      filtorder=3*fix(fs/30);
+    EEG = pop_eegfilt( EEG,1,30,filtorder,0,[],1);
+     pop_eegplot( EEG, 1, 1, 1);
+save(strcat(outDir,(strtok(cases(i).name,'.')),'filt.mat'),'EEG')
+    
+  
 end
