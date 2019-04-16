3d4f40ae21498bec03c7e0e639d16a7d4da38f7f
Gioch95 <46966372+Gioch95@users.noreply.github.com>
2019-04-16 11:37:31 +0200

    Add files via upload

---
+++ filter.m
@@ -0,0 +1,18 @@
+function EEG=filter(Y)
+
+inDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZ\';%cartella FNSZ
+%inDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZ\';%cartella GNSZ
+outDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZFilter\';
+%outDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZFilter\';
+fs=250;
+
+estensione='*.mat';
+cases=dir(fullfile(inDir,estensione));
+for i=1:length(cases)
+     load(strcat(inDir,cases(i).name));
+     filtorder=3*fix(fs/30);
+    EEG = pop_eegfilt( EEG,1,30,filtorder,0,[],1);
+     pop_eegplot( EEG, 1, 1, 1);
+save(strcat(outDir,(strtok(cases(i).name,'.')),'filt.mat'),'EEG')
+ close
+end
