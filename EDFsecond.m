bf0f7ddb780583423e2713f83acb46599177584a
Gioch95 <46966372+Gioch95@users.noreply.github.com>
2019-05-31 14:21:41 +0200

    Update EDFsecond.m

--- EDFsecond.m
+++ EDFsecond.m
@@ -3,8 +3,7 @@ load('G.mat');%matrice con 10 righe di GNSZ e due colonne
 
 %inDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZedf\';
 inDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZedf\';
-%inDir='C:\Users\gioiachiodi\Documents\MATLAB\FNSZedf30s\';
-%inDir='C:\Users\gioiachiodi\Documents\MATLAB\GNSZedf30s\';
+
 estensione='*.edf';
 cases=dir(fullfile(inDir,estensione));
 fs=250;
@@ -22,12 +21,9 @@ for i=1:length(cases)
              T1=fs*T;
               y=30*fs;
              EEG.data=EEG.data(:,T1-y:T1-1);
-         EEG.chanlocs=x;
+         
         end%a è la struttura %b è la matrice canalixcampioni %tempo di start seizure %x sono i 30 secondi
         pop_writeeeg(EEG,strcat(inDir,cases(i).name),'TYPE','EDF');
-    %filename = strcat(outDir,(strtok(cases(i).name,'.')),'fnsz30s');
-        %filename = strcat(outDir,(strtok(cases(i).name,'.')),'gnsz30s');
-    %save(filename,'EEG');
-
+   
  end
 end
